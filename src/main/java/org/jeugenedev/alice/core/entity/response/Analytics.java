8d9820c5bc5abc2c6f024dafa8702c82209aeeb0
Eugene <genyamateyuk@gmail.com>
2023-04-09 07:30:58 +0700

    analytics and response with states

--- src/main/java/org/jeugenedev/alice/core/entity/response/Analytics.java
+++ src/main/java/org/jeugenedev/alice/core/entity/response/Analytics.java
@@ -1,4 +1,56 @@
 package org.jeugenedev.alice.core.entity.response;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.util.List;
+
 public class Analytics {
+    private List<Event> events;
+
+    public Analytics() {
+    }
+
+    public Analytics(List<Event> events) {
+        this.events = events;
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    @Override
+    public String toString() {
+        return "Analytics{" +
+                "events=" + events +
+                '}';
+    }
+
+    static class Event {
+        private String name;
+        private JsonNode value;
+
+        public Event() {
+        }
+
+        public Event(String name, JsonNode value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getValue() {
+            return value.toString();
+        }
+
+        @Override
+        public String toString() {
+            return "Event{" +
+                    "name='" + name + '\'' +
+                    ", value=" + value +
+                    '}';
+        }
+    }
 }
