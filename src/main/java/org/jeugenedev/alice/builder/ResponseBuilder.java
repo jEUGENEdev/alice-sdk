d09fa985112e1817e5a89376fdd4145c07153333
Eugene <genyamateyuk@gmail.com>
2023-04-23 05:54:26 +0300

    added overview reponse (json item) builder

--- src/main/java/org/jeugenedev/alice/builder/ResponseBuilder.java
+++ src/main/java/org/jeugenedev/alice/builder/ResponseBuilder.java
@@ -1,5 +1,57 @@
 package org.jeugenedev.alice.builder;
 
-public final class ResponseBuilder {
-    ResponseBuilder() {}
+import org.jeugenedev.alice.core.entity.response.Response;
+
+public final class ResponseBuilder extends Returning<ServerResponseBuilder>
+        implements Builder.RequiredResponseItemBuilder, Builder.TextAndTtsResponseItemBuilder, Builder.EndSessionResponseItemBuilder, Builder.PostResponseItemBuilder, Builder.Assembled<Response> {
+    private Response response;
+    private String text, tts;
+    private boolean endSession;
+
+    ResponseBuilder(ServerResponseBuilder prev) {
+        super(prev);
+    }
+
+    @Override
+    public Builder.TextAndTtsResponseItemBuilder getRequiredResponseBuilder() {
+        return this;
+    }
+
+    @Override
+    public Builder.EndSessionResponseItemBuilder setData(String text, String tts) {
+        this.text = text;
+        this.tts = tts;
+        return this;
+    }
+
+    @Override
+    public Builder.PostResponseItemBuilder setEndSession(boolean end) {
+        this.endSession = end;
+        return this;
+    }
+
+    @Override
+    public Response assemble() {
+        return response;
+    }
+
+    @Override
+    public Builder.PostResponseItemBuilder buttons() {
+        return null;
+    }
+
+    @Override
+    public Builder.PostResponseItemBuilder itemMeta() {
+        return null;
+    }
+
+    @Override
+    public Builder.PostResponseItemBuilder card() {
+        return null;
+    }
+
+    @Override
+    public Builder.PostResponseItemBuilder directives() {
+        return null;
+    }
 }
--- src/main/java/org/jeugenedev/alice/builder/ServerResponseBuilder.java
+++ src/main/java/org/jeugenedev/alice/builder/ServerResponseBuilder.java
@@ -1,4 +1,46 @@
 package org.jeugenedev.alice.builder;
 
-public final class ServerResponseBuilder {
+import org.jeugenedev.alice.core.server.response.ServerResponse;
+
+public final class ServerResponseBuilder
+        implements Builder.SaveStatesResponseBuilder, Builder.Assembled<ServerResponse> {
+    private ServerResponse serverResponse;
+    private String version;
+    private boolean session, user, application;
+    private final ResponseBuilder response = new ResponseBuilder(this);
+
+    private ServerResponseBuilder(String version) {
+        this.version = version;
+    }
+
+    public static Builder.SaveStatesResponseBuilder of(Version version) {
+        return new ServerResponseBuilder(version.getVersion());
+    }
+
+    @Override
+    public Builder.RequiredResponseItemBuilder save(boolean session, boolean user, boolean application) {
+        this.session = session;
+        this.user = user;
+        this.application = application;
+        return this.response;
+    }
+
+    @Override
+    public ServerResponse assemble() {
+        return serverResponse;
+    }
+
+    public enum Version {
+        VERSION_1_0("1.0");
+
+        private final String version;
+
+        Version(String version) {
+            this.version = version;
+        }
+
+        public String getVersion() {
+            return version;
+        }
+    }
 }
